7668e08e8c8f016ad6c592db5080dda93f066bff
Kraznys Narwhal <knarwhal@vo-i2.21-school.ru>
2019-12-05 20:15:54 +0300

    debug

--- main.c
+++ main.c
@@ -10,7 +10,9 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int		checktouches(*char buf)
+#include "main.h"
+
+int		checktouches(char *buf)
 {
 	int		i;
 	int		count;
@@ -37,29 +39,29 @@ int		checktouches(*char buf)
 	return (0);
 }
 
-int		checkconsist(*char buf)
+int		checkconsist(char *buf)
 {
 	int		i;
 	int		hashcount;
 
 	i = 0;
 	hashcount = 0;
-	while (i < 19)
+	while (i < 20)
 	{
 		if (buf[i] != '\n' && buf[i] != '.' && buf[i] != '#')
 			return (0);
-		if (buf[i] == '\n' && !(i % 4))
+		if (buf[i] == '\n' && !(i % 5))
 			return (0);
 		if (buf[i] == '#')
 			hashcount++;
 	i++;
 	}
-	if (hashcount != 4 || !buf[i] || buf[i] != '\n')
+	if (hashcount != 4 || (buf[i] != '\0' && buf[i] != '\n'))
 		return (0);
 	return (1);
 }
 
-int		validate(*char buf, int bytesread)
+int		validate(char *buf, int bytesread)
 {
 	int		i;
 	int		tetramcount;
@@ -70,7 +72,7 @@ int		validate(*char buf, int bytesread)
 	{
 		if (!checkconsist(buf + i))
 			return (0);
-		if (!checktouches(buf + 1))
+		if (!checktouches(buf + i))
 			return (0);
 		i += 21;
 		tetramcount++;
@@ -78,38 +80,41 @@ int		validate(*char buf, int bytesread)
 	return (tetramcount);
 }
 
-int		parse(**char argv)
+int		parse(char *argv)
 {
 	int		fd;
 	int		bytesread;
 	char	buf[545];
 	int		tetranum;
 
-	fd = open(argv[1], O_RDONLY);
+	fd = open(argv, O_RDONLY);
 	bytesread = read(fd, buf, 545);
 	close (fd);
 	if (bytesread > 544 || bytesread < 19)
 		return (0);
 	buf[bytesread] = '\0';
-	if (!(tetranum = validate(buf, bytesread)));
+	if (!(tetranum = validate(buf, bytesread)))
 		return (0);
-	return (1);
+	return (tetranum);
 }
 
-int		main(int argc, **char argv)
+int		main(int argc, char **argv)
 {
+	int	test = 0;
+	
 	if (argc != 2)
 	{
 		ft_putstr("usage: ./fillit source_file\n");
 		return (0);
 	}
-	if (!parse(argv[1]))
+	if (!(test = parse(argv[1])))
 	{
 		ft_putstr("error\n");
 		return (0);
 	}
 	else
-		solve(arv[1]);
+		printf("%d", test);
+//		solve(arv[1]);
 	return (0);
 }
 
---
+++ main.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: knarwhal <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/05 19:10:57 by knarwhal          #+#    #+#             */
+/*   Updated: 2019/12/05 19:12:11 by knarwhal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <strings.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "libft/libft.h"
